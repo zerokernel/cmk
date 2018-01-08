94b09fbe2e468ff6004c4ad232a18c81c4a1a751
wangchenxunum <wangchenxunum@qq.com>
2018-01-08 21:59:23 +0800

    更新至v0.0.7

--- test/test.js
+++ test/test.js
@@ -1,38 +1,22 @@
 #!/usr/bin/env node
 'use strict';
-
-let run = (() => {
-	var _ref = _asyncToGenerator(function* () {
-		let db = yield _mongodb.MongoClient.connect('mongodb://localhost:27017/cmk');
-		const { create, set, remove, get, review, query } = yield (0, _2.default)(db.collection("list"));
-		let c = [];
-		for (let i = 0; i < 20; i++) {
-			c[i] = yield create({ title: "测试" + i, author: "作者" + i });
-			yield console.log(`c[${i}]:`, c[i]);
-		}
-		console.log((yield review(c[5])));
-		console.log((yield review(c[10], 11111)));
-		console.log((yield review(c[15])));
-		console.log((yield remove(c[11])));
-		console.log((yield remove(c[11], false)));
-		console.log((yield get(c[11])));
-		console.log((yield get(c[12])));
-		console.log((yield set(c[10], { title: "新标题", review: 123456 })));
-	});
-
-	return function run() {
-		return _ref.apply(this, arguments);
-	};
-})();
-
-var _mongodb = require('mongodb');
-
-var _ = require('../');
-
-var _2 = _interopRequireDefault(_);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
-run().catch(console.log).then(x => process.exit(0));
+const {MongoClient} = require("mongodb");
+const cmk = require('../');
+async function run () {
+	const db = (await MongoClient.connect('mongodb://localhost:27017/cmk')).db("cmk");
+	const {create, set, remove, get, review, query} = await cmk(db.collection("list"));
+	let c = [];
+	for(let i = 0; i < 20; i++) {
+		c[i] = await create({title:"测试" + i,author:"作者" + i});
+		await console.log(`c[${i}]:`, c[i]);
+	}
+	console.log(await review(c[5]));
+	console.log(await review(c[10],11111));
+	console.log(await review(c[15]));
+	console.log(await remove(c[11]));
+	console.log(await remove(c[11],false));
+	console.log(await get(c[11]));
+	console.log(await get(c[12]));
+	console.log(await set(c[10],{title:"新标题",review:123456}));
+}
+run().catch(console.log).then(x=>process.exit(0))
